5beb4616aa6b927e7eb2bd01b0f8937bed7f7411
ben <tunnello@163.com>
2021-11-08 14:33:52 +0800

    finish

--- autoString.py
+++ autoString.py
@@ -27,17 +27,46 @@ def excel_table_byindex(file=filename,):
         if oneRow[0]!="":
          list.append(oneRow)
 
-    unFindList=[]
+    findList=[]
 
     myJson = json.load(open("intl_en.json"))
+    rawJson=myJson
+    print("raw data  "+str(myJson))
     for findindex in myJson:
+        # print("取出资源文件中的数据  "+findindex)
         for value in list:
-            if value[0] == myJson[findindex]:
+            # print("取出excel中的数据  " + str(value))
+            if str(value[0]).strip()== str(myJson[findindex]).strip():
                 myJson[findindex]=value[1]
-            else:
-                unFindList.append(findindex)
+                findList.append(findindex)
+
+
     with open("newJson.json", 'w') as f:
         json.dump(myJson, f, ensure_ascii=False)
 
+    for value in findList:
+        rawJson.pop(value)
+
+    with open("unfind.json", 'w') as f:
+        json.dump(rawJson, f, ensure_ascii=False)
+
+
+
+def is_chinese(string):
+    """
+    检查整个字符串是否包含中文
+    :param string: 需要检查的字符串
+    :return: bool
+    """
+    for ch in string:
+        if u'\u4e00' <= ch <= u'\u9fff':
+            return True
+
+    return False
+
+
+
 excel_table_byindex("tanslation.xlsx")
+
+# print(is_chinese("ครบทั้งสี่มุม"))
 
